20a7a153d18703fedf7af0766b3aab7228e72da1
JoseDaniel1802 <124645280+JoseDaniel1802@users.noreply.github.com>
2024-05-03 13:05:15 -0600

    creacion y coneccion del boton que recibe la entrada de las matrices

--- op_matrices_window.py
+++ op_matrices_window.py
@@ -54,17 +54,17 @@ class Op_matrices_window(QWidget):
         self.rows_label.setFont(QFont("Arial", 10, QFont.Weight.Bold))
         rows_layout_1.addWidget(self.rows_label)
 
-        self.input_rows = QLineEdit()
-        self.input_rows.setStyleSheet("height: 23px; border-radius: 10px; border: 2px  white;")
-        rows_layout_1.addWidget(self.input_rows)
+        self.input_rows_1 = QLineEdit()
+        self.input_rows_1.setStyleSheet("height: 23px; border-radius: 10px; border: 2px  white;")
+        rows_layout_1.addWidget(self.input_rows_1)
 
         self.columns_label = QLabel("Ingresar numero de Columnas:")
         self.columns_label.setFont(QFont("Arial", 10, QFont.Weight.Bold))
         columns_layout_1.addWidget(self.columns_label)
 
-        self.input_columns = QLineEdit()
-        self.input_columns.setStyleSheet("height: 23px; border-radius: 10px; border: 2px  white;")
-        columns_layout_1.addWidget(self.input_columns)
+        self.input_columns_1 = QLineEdit()
+        self.input_columns_1.setStyleSheet("height: 23px; border-radius: 10px; border: 2px  white;")
+        columns_layout_1.addWidget(self.input_columns_1)
 
         self.matrix_2_text = QLabel("Matriz 2: ")
         self.matrix_2_text.setFont(QFont("Arial", 10, QFont.Weight.Bold))
@@ -92,12 +92,12 @@ class Op_matrices_window(QWidget):
         self.input_columns.setStyleSheet("height: 23px; border-radius: 10px; border: 2px  white;")
         columns_layout_2.addWidget(self.input_columns)
 
-
-
         self.confirm_button = QPushButton("Confirmar")
         self.confirm_button.setStyleSheet(
             "height: 30px; background-color: #a9e159; color: white; border: 2x solid black; border-radius: 13px;")
         self.confirm_button.setFont(QFont("Arial", 11))
+        self.confirm_button.clicked.connect(lambda: self.init_op_sistem(layout))
+
 
         matrix_1.addLayout(rows_layout_1)
         matrix_1.addLayout(columns_layout_1)
@@ -108,6 +108,56 @@ class Op_matrices_window(QWidget):
         dimensions_layout.addLayout(matrix_2)
         option_layout.addLayout(dimensions_layout)
         option_layout.addWidget(self.confirm_button)
+
+
+        layout.addLayout(encabezado_layout)
+        layout.addLayout(option_layout)
+        self.setLayout(layout)
+
+    def create_matrix_input_1(self, layout: QVBoxLayout):
+        matrix_layout = QVBoxLayout()
+        rows = int(self.input_rows_1.text())
+        columns = int(self.input_columns_1.text())
+        self.matrix_1_ = QLabel("Matriz 1: ")
+        self.matrix_1_.setFont(QFont("Arial", 10, QFont.Weight.Bold))
+        matrix_layout.addWidget(self.matrix_1_)
+
+        for _ in range(rows):
+            row_layout = QHBoxLayout()
+            for _ in range(columns):
+                self.input_row = QLineEdit()
+                self.input_row.setStyleSheet("height: 23px; border-radius: 10px; border: 2px  white;")
+                row_layout.addWidget(self.input_row)
+
+            matrix_layout.addLayout(row_layout)
+
+        layout.addLayout(matrix_layout)
+
+    def create_matrix_input_2(self, layout: QVBoxLayout):
+        matrix_layout = QVBoxLayout()
+        rows = int(self.input_rows.text())
+        columns = int(self.input_columns.text())
+        self.matrix_2_ = QLabel("Matriz 2: ")
+        self.matrix_2_.setFont(QFont("Arial", 10, QFont.Weight.Bold))
+        matrix_layout.addWidget(self.matrix_2_)
+
+        for _ in range(rows):
+            row_layout = QHBoxLayout()
+            for _ in range(columns):
+                self.input_row = QLineEdit()
+                self.input_row.setStyleSheet("height: 23px; border-radius: 10px; border: 2px  white;")
+                row_layout.addWidget(self.input_row)
+
+            matrix_layout.addLayout(row_layout)
+
+        layout.addLayout(matrix_layout)
+
+    def init_op_sistem(self, layout: QVBoxLayout):
+
+        self.setGeometry(550, 90, 450, 400)
+
+        self.create_matrix_input_1(layout)
+        self.create_matrix_input_2(layout)
 
         self.sum_button = QPushButton("Sumar")
         self.sum_button.setStyleSheet(
@@ -129,15 +179,11 @@ class Op_matrices_window(QWidget):
             "height: 30px; background-color: #a9e159; color: white; border: 2x solid black; border-radius: 13px;")
         self.product_button.setFont(QFont("Arial", 11))
 
-        layout.addLayout(encabezado_layout)
-        layout.addLayout(option_layout)
-        layout.addWidget(self.matrix_1_text)
         layout.addWidget(self.matrix_2_text)
         layout.addWidget(self.sum_button)
         layout.addWidget(self.rest_button)
         layout.addWidget(self.multiplicacion_button)
         layout.addWidget(self.product_button)
-        self.setLayout(layout)
 
 
 if __name__ == "__main__":
